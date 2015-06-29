1f40ca5615f23b3725f8a8988c5aecb024cad324
Totoro le Pacha <totorolepacha@gmail.com>
2015-06-29 18:47:55 +0200

    Initial Commit (CA POSTE LE DESSERT AAA)

---
+++ test.py
@@ -0,0 +1,3 @@
+import twitter
+
+twitter.post_tweet("LE DESSERT", None)
---
+++ twitter_settings.py
@@ -0,0 +1,5 @@
+import oauth2 as oauth
+
+screen_name='' # Twitter username
+token = oauth.Token(key="", secret="") # Twitter user token and secret
+consumer = oauth.Consumer(key="", secret="") # Twitter appliction key and secret
