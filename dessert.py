c5de4d20785add8b8f3cb0b3be4bbb092b2f3c1a
Totoro le Pacha <totorolepacha@gmail.com>
2015-06-30 17:25:05 +0200

    Tweet aléatoire fonctionnel

--- dessert.py
+++ dessert.py
@@ -1,3 +1,33 @@
-import twitter
+# coding: utf8
 
-twitter.post_tweet("LE DESSERT", None)
+import sys
+import tweets
+
+def print_usage() :
+	print("USAGE : python dessert.py [argument]\n\tstatut : Envoie le statut dessert\n\tmentions : Analyse les mentions\n\tmessages : Répond aux messages privés")
+	
+
+if len(sys.argv) != 2 :
+	print_usage()
+	sys.exit(0)
+else:
+	if sys.argv[1] == "statut":
+		print("Post du statut débuté")
+		tweets.process_status()
+		print("Post du statut complété.")
+		
+	elif sys.argv[1] == "mentions":
+		print("Traitement des mentions en cours...")
+		tweets.process_mentions()
+		print("Traitement des mentions complété.")
+		
+	elif sys.argv[1] == "messages":
+		print("Traitement des messages privés en cours...")
+		tweets.process_mps()
+		print("Traitement des messages privés complété.")
+		
+	else:
+		print_usage()
+	
+	
+	
--- twitter_settings.py
+++ twitter_settings.py
@@ -1,5 +1,5 @@
 import oauth2 as oauth
 
-screen_name='' # Twitter username
-token = oauth.Token(key="", secret="") # Twitter user token and secret
-consumer = oauth.Consumer(key="", secret="") # Twitter appliction key and secret
+screen_name='_LEDESSERT_' # Twitter username
+token = oauth.Token(key="258079456-LcRgLMv3n4yklJxcsTf66zwXEjZJuETqvBPVxNHF", secret="3SZZI5rMcSZWkfQNaBvCr0sLqtz83RcvyTPuId5G774HQ") # Twitter user token and secret
+consumer = oauth.Consumer(key="35qlOrd3863HIhkVFWHADBhoq", secret="JVOWXYpLDPFAXshkAzbB7EqP7RouU7d3thHfDgBRs854sSS68e") # Twitter appliction key and secret
